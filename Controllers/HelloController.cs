f5dacb23f25afe29c759422af042fab46662087b
dennis-phillips <dennis_phillips@outlook.com>
2020-06-15 13:50:07 -0400

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASP.NET.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        [Route("/helloworld/")]
+        public IActionResult Index()
+        {
+            string html = "<h1>Hello World!</h1>";
+            return Content(html, "text/html");
+        }
+    }
+}
