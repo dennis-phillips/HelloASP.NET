2a8add42053d1e15ed5e63fd1ba17cfb436aeb58
dennis-phillips <dennis_phillips@outlook.com>
2020-06-16 12:29:28 -0400

    Unit 10 Exercises complete

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -3,16 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HelloASP.NET.Controllers
 {
+    [Route("/helloworld/")]
     public class HelloController : Controller
     {
         [HttpGet]
-        [Route("/helloworld/")]
+        
         public IActionResult Index()
         {
-            string html = "<h1>Hello World!</h1>";
+            string html = "<form method = 'post' action = '/CreateMessage/'>" +
+                 "<input type = 'text' name = 'name'/>" +
+                 "<select name = 'language'>" +
+                 "<option value=''>--Please choose an option--</option> " +
+                  "<option value='Hello! '> English </option> " +
+
+                 "<option value='Bon Jour! '> French </option> " +
+
+                 "<option value='Hola! '> Spanish </option> " +
+
+                 "<option value='Zdravo! '> Serbian </option> " +
+
+                 "<option value='Guten Tag! '> German </option> " +
+                 "</select>" +
+                 "<input type='submit' value = 'Greet Me!'/>" +
+                 "</form>";
+            return Content(html, "text/html");
+        }
+
+        //GET: /hello/welcome
+        //[HttpGet]
+        //[Route("/hello/welcome/{name?}")]
+        [HttpGet("welcome/{name?}")]
+        [HttpPost]
+        
+        public IActionResult Welcome(string name = "Whoever you are")
+        {
+            return Content("<h1> Welcome to my App " + name + "!<h/1", "text/html");
+        }
+
+        [HttpPost("/CreateMessage/")]
+        public IActionResult CreateMessage( string name, string language= "English")
+        {
+            string html = language + name;
             return Content(html, "text/html");
         }
     }
