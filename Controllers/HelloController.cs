ce8cb099c4430a169d2c7d70ca2008e0c3d36a1e
dennis-phillips <dennis_phillips@outlook.com>
2020-06-17 12:32:44 -0400

    Unit 13 walkthrough up to 13.6 razor forms

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -14,23 +14,8 @@ namespace HelloASP.NET.Controllers
         
         public IActionResult Index()
         {
-            string html = "<form method = 'post' action = '/CreateMessage/'>" +
-                 "<input type = 'text' name = 'name'/>" +
-                 "<select name = 'language'>" +
-                 "<option value=''>--Please choose an option--</option> " +
-                  "<option value='Hello! '> English </option> " +
-
-                 "<option value='Bon Jour! '> French </option> " +
-
-                 "<option value='Hola! '> Spanish </option> " +
-
-                 "<option value='Zdravo! '> Serbian </option> " +
-
-                 "<option value='Guten Tag! '> German </option> " +
-                 "</select>" +
-                 "<input type='submit' value = 'Greet Me!'/>" +
-                 "</form>";
-            return Content(html, "text/html");
+            
+            return View();
         }
 
         //GET: /hello/welcome
@@ -41,7 +26,8 @@ namespace HelloASP.NET.Controllers
         
         public IActionResult Welcome(string name = "Whoever you are")
         {
-            return Content("<h1> Welcome to my App " + name + "!<h/1", "text/html");
+            ViewBag.person = name;
+            return View();
         }
 
         [HttpPost("/CreateMessage/")]
